44d551677ff21d26d9c1f2cbda9a44797ef7954c
subramanian2401 <112458112+subramanian2401@users.noreply.github.com>
2023-04-14 14:05:46 +0530

    Update visual22.py

--- visual22.py
+++ visual22.py
@@ -2,6 +2,12 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
+st.markdown(
+    """
+    <h1 style='text-align: center;'>Sales Forcasting</h1>
+    """,
+    unsafe_allow_html=True
+)
 # Add a browse file button to upload a CSV file
 uploaded_file = st.file_uploader(r'C:\Users\subra\Documents\4th semester\Mca 5964 Project\project process\sorted3.csv', type="csv")
 
@@ -13,8 +19,26 @@ if uploaded_file is not None:
     x_axis = st.selectbox("Select a column to plot on the x-axis", data.columns)
     y_axis = st.selectbox("Select a column to plot on the y-axis", data.columns)
 
-    # Create a scatter plot
-    plt.scatter(data[x_axis], data[y_axis])
+    import numpy as np
+
+
+
+    # Create a figure and axis object
+    fig, ax = plt.subplots()
+
+    # Plot the data with a color map
+    sc = ax.scatter(data[x_axis], data[y_axis], c='r', cmap=plt.cm.jet)
+
+    # Add a color bar
+    cbar = fig.colorbar(sc)
+
+    # Set the chart title and labels
+    ax.set_title('Sine Wave')
+    ax.set_xlabel('X-axis')
+    ax.set_ylabel('Y-axis')
+
+    #Create a scatter plot
+    plt.scatter(data[x_axis], data[y_axis],color='#b3f542')
     plt.xlabel(x_axis)
     plt.ylabel(y_axis)
-    st.pyplot()
+    st.pyplot(fig)
